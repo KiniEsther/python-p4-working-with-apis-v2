f855f4907b0fcefc9102fe7704a3612405ba6e44
Brown <esther.kini@student.moringaschool.com>
2024-10-03 09:37:58 +0300

    initial commit

--- lib/open_library_api.py
+++ lib/open_library_api.py
@@ -1,57 +1,30 @@
+
+
 import requests
 import json
 
+class GetPrograms:
 
-class Search:
+  def get_programs(self):
+    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
 
-    def get_search_results(self):
-        search_term = "the lord of the rings"
+    response = requests.get(URL)
+    return response.content
 
-        search_term_formatted = search_term.replace(" ", "+")
-        fields = ["title", "author_name"]
-        # formats the list into a comma separated string
-        # output: "title,author_name"
-        fields_formatted = ",".join(fields)
-        limit = 1
+  def program_agencies(self):
+    # we use the JSON library to parse the API response into nicely formatted JSON
+        programs_list = []
+        programs = json.loads(self.get_programs())
+        for program in programs:
+            programs_list.append(program["agency"])
 
-        URL = f"https://openlibrary.org/search.json?title={search_term_formatted}&fields={fields_formatted}&limit={limit}"
+        return programs_list
 
-        response = requests.get(URL)
-        return response.content
+#programs = GetPrograms().get_programs()
+#print(programs)
 
-    def get_search_results_json(self):
-        search_term = "the lord of the rings"
+programs = GetPrograms()
+agencies = programs.program_agencies()
 
-        search_term_formatted = search_term.replace(" ", "+")
-        fields = ["title", "author_name"]
-        fields_formatted = ",".join(fields)
-        limit = 1
-
-        URL = f"https://openlibrary.org/search.json?title={search_term_formatted}&fields={fields_formatted}&limit={limit}"
-        print(URL)
-        response = requests.get(URL)
-        return response.json()
-
-    def get_user_search_results(self, search_term):
-        search_term_formatted = search_term.replace(" ", "+")
-        fields = ["title", "author_name"]
-        fields_formatted = ",".join(fields)
-        limit = 1
-
-        URL = f"https://openlibrary.org/search.json?title={search_term_formatted}&fields={fields_formatted}&limit={limit}"
-
-        response = requests.get(URL).json()
-        response_formatted = f"Title: {response['docs'][0]['title']}\nAuthor: {response['docs'][0]['author_name'][0]}"
-        return response_formatted
-
-
-# results = Search().get_search_results()
-# print(results)
-
-# results_json = Search().get_search_results_json()
-# print(json.dumps(results_json, indent=1))
-
-search_term = input("Enter a book title: ")
-result = Search().get_user_search_results(search_term)
-print("Search Result:\n")
-print(result)
+for agency in set(agencies):
+    print(agency) 
